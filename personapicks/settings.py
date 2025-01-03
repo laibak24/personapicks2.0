66cb50f2992713acf8edd37901fea5db24c45600
Laiba Khan <klaiba2406@gmail.com>
2025-01-03 04:45:56 -0800

    Deploy Try 5

--- personapicks/settings.py
+++ personapicks/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR,'website/static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','website/static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
